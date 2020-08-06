ec5546ab7fd7d83aae5f4ea4543e1439712d2118
Mark Gray <mark-git@vatavia.net>
2020-08-06 17:33:20 -0400

    story_formats:   authenticate except: [:index, :show]   allow upload of story to extract format from it.   If header is a whole story, parse apart header and footer, get name, version from story views/stories/_form: optimization: comparing user IDs faster than users

--- app/controllers/story_formats_controller.rb
+++ app/controllers/story_formats_controller.rb
@@ -1,4 +1,5 @@
 class StoryFormatsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_story_format, only: [:show, :edit, :update, :destroy]
 
   # GET /story_formats
@@ -25,6 +26,29 @@ class StoryFormatsController < ApplicationController
   # POST /story_formats.json
   def create
     @story_format = StoryFormat.new(story_format_params)
+    upload = params[:story_format][:uploaded_story]
+    if upload
+      if upload.content_type.chomp != "text/html"
+        redirect_to(action: 'new', notice: 'Upload must be text/html.')
+        return
+      end
+      @story_format.header = upload.read.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')
+    end
+
+    # If .header contains a story, remove the story and move the part after it into .footer
+    story_start = @story_format.header.index("<tw-storydata ")
+    if story_start
+      story_end_tag = "</tw-storydata>"
+      story_end = @story_format.header.index(story_end_tag, story_start)
+      if story_end
+        found_format = attr_value('format', @story_format.header, story_start)
+        found_version = attr_value('format-version', @story_format.header, story_start)
+        @story_format.name = found_format if found_format
+        @story_format.version = found_version if found_version
+        @story_format.footer = @story_format.header[story_end + story_end_tag.length..]
+      end
+      @story_format.header = @story_format.header[0..story_start-1]
+    end
 
     respond_to do |format|
       if @story_format.save
@@ -56,19 +80,28 @@ class StoryFormatsController < ApplicationController
   def destroy
     @story_format.destroy
     respond_to do |format|
-      format.html { redirect_to story_formats_url, notice: 'Story format was successfully destroyed.' }
+      format.html { redirect_to story_formats_url, notice: 'Story format was successfully deleted.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_story_format
       @story_format = StoryFormat.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def story_format_params
       params.require(:story_format).permit(:name, :version, :author, :header, :footer)
     end
+
+    def attr_value(attr_name, str, start)
+      search_for = ' ' + attr_name + '="'
+      attr_start = str.index(search_for, start)
+      if attr_start
+        attr_end = str.index('"', attr_start + search_for.length)
+        if attr_end
+          @story_format.header[attr_start + search_for.length..attr_end - 1]
+        end
+      end
+    end        
 end
